3bf5596f999019060c4c068ee5f328a8178f9112
Batman-S <83809552+Batman-S@users.noreply.github.com>
2021-09-08 09:49:01 +0900

    added dir name

--- server/knex.js
+++ server/knex.js
@@ -7,7 +7,7 @@ const db = knex({
     process.env.DATABASE_URL ||
     `postgres://${process.env.USER}@127.0.0.1:5432/starbelly`,
   migration: {
-    directory: "../migrations",
+    directory: __dirname + "/migrations",
   },
   searchPath: "public",
 });
